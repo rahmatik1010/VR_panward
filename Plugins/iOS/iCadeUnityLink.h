e8e351c2d8493fbb5f6b8e163228bfd844ffe595
rahmatik1010 <49456283+rahmatik1010@users.noreply.github.com>
2019-04-10 03:40:36 +0700

    Add files via upload

---
+++ Plugins/iOS/iCadeUnityLink.h
@@ -0,0 +1,16 @@
+//
+//  iCadeUnityLink.h
+//  Unity-iPhone
+//
+//  Created by Matthew Klundt on 12/11/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "iCadeReaderView.h"
+
+@interface iCadeUnityLink : NSObject <iCadeEventDelegate> {
+	int joystickNum;
+}
+
+@end
